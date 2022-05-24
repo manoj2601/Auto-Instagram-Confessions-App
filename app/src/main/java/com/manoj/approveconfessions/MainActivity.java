244f93dd59c8594818acb3a3a92936b370d52f1c
Manoj Kumar <manoj18.iitd@gmail.com>
2022-05-24 16:35:45 +0530

    approve frontend and back button created

--- app/src/main/java/com/manoj/approveconfessions/MainActivity.java
+++ app/src/main/java/com/manoj/approveconfessions/MainActivity.java
@@ -1,14 +1,36 @@
 package com.manoj.approveconfessions;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // calling the action bar
+        ActionBar actionBar = getSupportActionBar();
+
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Toast.makeText(MainActivity.this, "Back Button Pressed", Toast.LENGTH_SHORT).show();
+//                expiresession();
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
